6575c31108e5684e0176cb917555904273c0f1af
Scill <realscill@gmail.com>
2023-04-14 11:35:10 -0400

    Refactor plugin

--- midnight-example/src/main/java/me/colingrimes/example/ExamplePlugin.java
+++ midnight-example/src/main/java/me/colingrimes/example/ExamplePlugin.java
@@ -1,13 +1,13 @@
 package me.colingrimes.example;
 
 import me.colingrimes.example.listener.PlayerListeners;
-import me.colingrimes.midnight.plugin.Midnight;
+import me.colingrimes.midnight.plugin.MidnightPlugin;
 import org.bukkit.event.Listener;
 
 import javax.annotation.Nonnull;
 import java.util.List;
 
-public class ExamplePlugin extends Midnight {
+public class ExamplePlugin extends MidnightPlugin {
 
 	@Override
 	protected void registerListeners(@Nonnull List<? super Listener> listeners) {
