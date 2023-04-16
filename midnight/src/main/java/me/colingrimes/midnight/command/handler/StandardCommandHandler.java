e461f7e20ee5814ea2e0d8c88909030d717ce4c1
Scill <realscill@gmail.com>
2023-04-16 17:51:18 -0400

    Add standard command handlers

---
+++ midnight/src/main/java/me/colingrimes/midnight/command/handler/StandardCommandHandler.java
@@ -0,0 +1,61 @@
+package me.colingrimes.midnight.command.handler;
+
+import me.colingrimes.midnight.command.Command;
+import me.colingrimes.midnight.command.argument.ArgumentList;
+import me.colingrimes.midnight.plugin.MidnightPlugin;
+import me.colingrimes.plugin.config.Settings;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+
+/**
+ * A standard implementation of the {@link CommandHandler} interface.
+ * This class handles the execution and tab completion of commands
+ * for a given {@link Command} and {@link MidnightPlugin} instance.
+ *
+ * <p>It checks for sender requirements, permissions, and the number of arguments
+ * before executing the command. Appropriate messages are sent to the sender
+ * based on these conditions.</p>
+ *
+ * @param <T> the type of the MidnightPlugin
+ */
+public class StandardCommandHandler<T extends MidnightPlugin> implements CommandHandler {
+
+	private final T plugin;
+	private final Command<T> command;
+
+	public StandardCommandHandler(@Nonnull T plugin, @Nonnull Command<T> command) {
+		this.plugin = plugin;
+		this.command = command;
+	}
+
+	@Override
+	public boolean onCommand(@Nonnull CommandSender sender, @Nonnull org.bukkit.command.Command cmd, @Nonnull String label, @Nonnull String[] args) {
+		if (command.isPlayerRequired() && !(sender instanceof Player)) {
+			Settings.INVALID_SENDER.sendTo(sender);
+			return true;
+		} else if (command.getPermission() != null && !sender.hasPermission(command.getPermission())) {
+			Settings.PERMISSION_DENIED.sendTo(sender);
+			return true;
+		} else if (args.length < command.getArgumentsRequired()) {
+			command.getUsage().sendTo(sender);
+			return true;
+		}
+
+		ArgumentList argumentList = new ArgumentList(args);
+		if (!command.execute(plugin, sender, argumentList)) {
+			command.getUsage().sendTo(sender);
+		}
+
+		return true;
+	}
+
+	@Nullable
+	@Override
+	public List<String> onTabComplete(@Nonnull CommandSender sender, @Nonnull org.bukkit.command.Command cmd, @Nonnull String label, @Nonnull String[] args) {
+		return command.tabComplete(plugin, sender, new ArgumentList(args));
+	}
+}
--- midnight/src/main/java/me/colingrimes/midnight/command/handler/factory/CommandHandlerFactory.java
+++ midnight/src/main/java/me/colingrimes/midnight/command/handler/factory/CommandHandlerFactory.java
@@ -1,12 +1,39 @@
 package me.colingrimes.midnight.command.handler.factory;
 
-import me.colingrimes.midnight.command.handler.AnnotationCommandHandler;
+import me.colingrimes.midnight.command.Command;
+import me.colingrimes.midnight.command.handler.CommandHandler;
+import me.colingrimes.midnight.command.handler.ReflectiveCommandHandler;
+import me.colingrimes.midnight.command.handler.StandardCommandHandler;
+import me.colingrimes.midnight.plugin.MidnightPlugin;
 
 import javax.annotation.Nonnull;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+/**
+ * The {@code CommandHandlerFactory} is a utility interface that provides static factory methods
+ * for creating {@code CommandHandler} instances. It simplifies the creation of both
+ * {@code StandardCommandHandler} and {@code ReflectiveCommandHandler} instances.
+ *
+ * <p>Example usage:</p>
+ * <pre>
+ * {@code
+ * CommandHandler standardHandler = CommandHandlerFactory.create(command);
+ * CommandHandler reflectiveHandler = CommandHandlerFactory.create(method, permission, usageMessage);
+ * }
+ * </pre>
+ */
 public interface CommandHandlerFactory {
+
+	/**
+	 * Creates a new command handler.
+	 * @param command the command
+	 * @return the command handler
+	 */
+	@Nonnull
+	static <T extends MidnightPlugin> CommandHandler create(@Nonnull T plugin, @Nonnull Command<T> command) {
+		return new StandardCommandHandler<>(plugin, command);
+	}
 
 	/**
 	 * Creates a new command handler.
@@ -16,8 +43,8 @@ public interface CommandHandlerFactory {
 	 * @return the command method
 	 */
 	@Nonnull
-	static AnnotationCommandHandler create(@Nonnull Method method, @Nonnull String permission, @Nonnull String usageMessage) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+	static CommandHandler create(@Nonnull Method method, @Nonnull String permission, @Nonnull String usageMessage) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 		Object instance = method.getDeclaringClass().getDeclaredConstructor().newInstance();
-		return new AnnotationCommandHandler(method, instance, permission, usageMessage);
+		return new ReflectiveCommandHandler(method, instance, permission, usageMessage);
 	}
 }
