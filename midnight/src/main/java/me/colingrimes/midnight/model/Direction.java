f0d2380d65e63d7950c55d07d4851747363490a1
Scill <realscill@gmail.com>
2023-04-19 01:20:30 -0400

    Switch yaw and pitch

--- midnight/src/main/java/me/colingrimes/midnight/model/Direction.java
+++ midnight/src/main/java/me/colingrimes/midnight/model/Direction.java
@@ -5,33 +5,25 @@ import javax.annotation.Nullable;
 import java.util.Objects;
 
 /**
- * Represents a 3D direction in the form of pitch and yaw angles.
+ * Represents a 3D direction in the form of yaw and pitch angles.
  */
 public class Direction {
 
-    private final double pitch;
     private final double yaw;
+    private final double pitch;
 
     /**
-     * Constructs a new Direction with the given pitch and yaw angles.
-     * @param pitch The pitch angle.
-     * @param yaw   The yaw angle.
+     * Constructs a new Direction with the given yaw and pitch angles.
+     * @param yaw the yaw angle
+     * @param pitch the pitch angle
      */
-    public static Direction of(double pitch, double yaw) {
-        return new Direction(pitch, yaw);
+    public static Direction of(double yaw, double pitch) {
+        return new Direction(yaw, pitch);
     }
 
-    Direction(double pitch, double yaw) {
-        this.pitch = pitch;
+    Direction(double yaw, double pitch) {
         this.yaw = yaw;
-    }
-
-    /**
-     * Gets the pitch angle.
-     * @return the pitch angle
-     */
-    public double getPitch() {
-        return pitch;
+        this.pitch = pitch;
     }
 
     /**
@@ -42,25 +34,33 @@ public class Direction {
         return yaw;
     }
 
+    /**
+     * Gets the pitch angle.
+     * @return the pitch angle
+     */
+    public double getPitch() {
+        return pitch;
+    }
+
     @Override
     public boolean equals(@Nullable Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Direction direction = (Direction) o;
-        return Double.compare(direction.getPitch(), getPitch()) == 0 && Double.compare(direction.getYaw(), getYaw()) == 0;
+        return Double.compare(direction.getYaw(), getYaw()) == 0 && Double.compare(direction.getPitch(), getPitch()) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getPitch(), getYaw());
+        return Objects.hash(getYaw(), getPitch());
     }
 
     @Nonnull
     @Override
     public String toString() {
         return "Direction{" +
-                "pitch=" + pitch +
-                ", yaw=" + yaw +
+                "yaw=" + yaw +
+                ", pitch=" + pitch +
                 '}';
     }
 }
