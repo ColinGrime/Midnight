25f8410a02eb778d867de18ef688f0bc49796318
Scill <realscill@gmail.com>
2023-04-19 10:44:14 -0400

    Add /particle clear

--- midnight/src/main/java/me/colingrimes/plugin/command/particle/clear/ParticleClear.java
+++ midnight/src/main/java/me/colingrimes/plugin/command/particle/clear/ParticleClear.java
@@ -2,6 +2,9 @@ package me.colingrimes.plugin.command.particle.clear;
 
 import me.colingrimes.midnight.command.Command;
 import me.colingrimes.midnight.command.util.CommandProperties;
+import me.colingrimes.midnight.command.util.Sender;
+import me.colingrimes.midnight.command.util.argument.ArgumentList;
+import me.colingrimes.midnight.particle.ParticleEffect;
 import me.colingrimes.plugin.Midnight;
 import me.colingrimes.plugin.config.Messages;
 
@@ -10,7 +13,13 @@ import javax.annotation.Nonnull;
 public class ParticleClear implements Command<Midnight> {
 
 	@Override
+	public void execute(@Nonnull Midnight plugin, @Nonnull Sender sender, @Nonnull ArgumentList args) {
+		plugin.getParticleManager().getSelectedParticle(sender.player()).ifPresent(ParticleEffect::stopSpawning);
+	}
+
+	@Override
 	public void configureProperties(@Nonnull CommandProperties properties) {
 		properties.setUsage(Messages.PARTICLE_CLEAR_USAGE);
+		properties.setPlayerRequired(true);
 	}
 }
