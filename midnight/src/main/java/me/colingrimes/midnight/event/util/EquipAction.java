8d032f89022d7aadeccf8b55df54cdb15317bd2d
Scill <realscill@gmail.com>
2023-04-26 19:26:38 -0400

    Add armor equip action

---
+++ midnight/src/main/java/me/colingrimes/midnight/event/util/EquipAction.java
@@ -0,0 +1,21 @@
+package me.colingrimes.midnight.event.util;
+
+/**
+ * An enumeration representing actions related to armor equipping.
+ */
+public enum EquipAction {
+    /**
+     * The action of equipping armor.
+     */
+    EQUIP,
+
+    /**
+     * The action of unequipping armor.
+     */
+    UNEQUIP,
+
+    /**
+     * The action of swapping armor.
+     */
+    SWAP
+}
