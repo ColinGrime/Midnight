f98d1845a4ea6cc609295bf59aa5c7cf1d0fc478
Scill <realscill@gmail.com>
2023-04-27 16:50:18 -0400

    Clean up queries

--- midnight/src/main/java/me/colingrimes/midnight/storage/sql/SchemaReader.java
+++ midnight/src/main/java/me/colingrimes/midnight/storage/sql/SchemaReader.java
@@ -6,16 +6,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public final class SchemaReader {
 
 	@Nonnull
-	public static List<String> getQueries(@Nonnull InputStream is) {
+	public static List<String> getQueries(@Nonnull InputStream is) throws IOException {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
 			String query = reader.lines().collect(Collectors.joining("\n"));
-			return List.of(query.split(";"));
+			return Arrays.stream(query.split(";"))
+					.map(String::trim)
+					.filter(s -> !s.isEmpty())
+					.collect(Collectors.toList());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
