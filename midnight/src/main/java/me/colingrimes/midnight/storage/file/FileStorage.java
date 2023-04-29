5f27c5f9a9f01fcce0062ee256c61126bddeab11
Scill <realscill@gmail.com>
2023-04-29 18:04:35 -0400

    Add loadData() method to post-process storage data

--- midnight/src/main/java/me/colingrimes/midnight/storage/file/FileStorage.java
+++ midnight/src/main/java/me/colingrimes/midnight/storage/file/FileStorage.java
@@ -1,17 +1,19 @@
 package me.colingrimes.midnight.storage.file;
 
 import me.colingrimes.midnight.MidnightPlugin;
+import me.colingrimes.midnight.serialize.Serializable;
 import me.colingrimes.midnight.storage.Storage;
 
 import javax.annotation.Nonnull;
 import java.io.File;
+import java.util.Map;
 import java.util.Optional;
 
 /**
  * An abstract class representing a file-based storage.
  * @param <T> the type of data being stored
  */
-public abstract class FileStorage<T> implements Storage<T> {
+public abstract class FileStorage<T extends Serializable> implements Storage<T> {
 
     protected final MidnightPlugin plugin;
     protected File file;
@@ -36,4 +38,13 @@ public abstract class FileStorage<T> implements Storage<T> {
     public void shutdown() {
         // No specific shutdown operations needed for file-based storage.
     }
+
+    /**
+     * Loads the data from the in-memory data map.
+     * Implementations of this method should perform any necessary
+     * post-processing on the data after it has been loaded.
+     *
+     * @param dataMap the in-memory data map containing the loaded data
+     */
+    protected abstract void loadData(@Nonnull Map<String, T> dataMap);
 }
