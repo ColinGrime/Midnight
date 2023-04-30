68096372ab2cba9c814ac20e178a820a86c6575b
Scill <realscill@gmail.com>
2023-04-29 22:44:19 -0400

    Add map of all particles

--- midnight/src/main/java/me/colingrimes/midnight/particle/ParticleManager.java
+++ midnight/src/main/java/me/colingrimes/midnight/particle/ParticleManager.java
@@ -6,10 +6,45 @@ import javax.annotation.Nonnull;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 public class ParticleManager {
 
+    private final Map<UUID, ParticleEffect> particleEffects = new HashMap<>();
     private final Map<Player, ParticleEffect> selectedParticles = new HashMap<>();
+
+    /**
+     * Gets the particle effect with the given name.
+     * @param name the name of the particle effect
+     * @return the particle effect, if any
+     */
+    @Nonnull
+    public Optional<ParticleEffect> getParticle(@Nonnull String name) {
+        for (ParticleEffect particle : particleEffects.values()) {
+            if (particle.getName().equalsIgnoreCase(name)) {
+                return Optional.of(particle);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Adds a particle effect.
+     * @param particle the particle effect to add
+     */
+    public void addParticle(@Nonnull ParticleEffect particle) {
+        particleEffects.put(particle.getUUID(), particle);
+    }
+
+    /**
+     * Loads the particle effects.
+     * @param particles the particle effects to load
+     */
+    public void loadParticles(@Nonnull Map<String, ParticleEffect> particles) {
+        for (ParticleEffect particle : particles.values()) {
+            addParticle(particle);
+        }
+    }
 
     /**
      * Gets the selected particle effect for a player.
@@ -27,6 +62,9 @@ public class ParticleManager {
      * @param particleEffect the particle effect that was selected
      */
     public void selectParticle(@Nonnull Player player, @Nonnull ParticleEffect particleEffect) {
+        if (getParticle(particleEffect.getName()).isEmpty()) {
+            addParticle(particleEffect);
+        }
         selectedParticles.put(player, particleEffect);
     }
 }
