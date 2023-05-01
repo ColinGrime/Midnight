fcbba1fa3300593e70030ea0ec3582d46bc9b8d9
Scill <realscill@gmail.com>
2023-05-01 13:39:07 -0400

    Spawn the selected particle if applicable

--- midnight/src/main/java/me/colingrimes/plugin/command/particle/spawn/ParticleSpawn.java
+++ midnight/src/main/java/me/colingrimes/plugin/command/particle/spawn/ParticleSpawn.java
@@ -4,7 +4,9 @@ import me.colingrimes.midnight.command.Command;
 import me.colingrimes.midnight.command.util.CommandProperties;
 import me.colingrimes.midnight.command.util.Sender;
 import me.colingrimes.midnight.command.util.argument.ArgumentList;
+import me.colingrimes.midnight.locale.Placeholders;
 import me.colingrimes.midnight.particle.ParticleEffect;
+import me.colingrimes.midnight.util.text.Text;
 import me.colingrimes.plugin.Midnight;
 import me.colingrimes.plugin.config.Messages;
 
@@ -15,6 +17,22 @@ public class ParticleSpawn implements Command<Midnight> {
 
 	@Override
 	public void execute(@Nonnull Midnight plugin, @Nonnull Sender sender, @Nonnull ArgumentList args) {
+		if (args.size() == 0) {
+			if (sender.isPlayer()) {
+				Optional<ParticleEffect> particle = plugin.getParticleManager().getSelectedParticle(sender.player());
+				if (particle.isPresent()) {
+					particle.get().startSpawning();
+					Messages.PARTICLE_SPAWN.sendTo(sender);
+					return;
+				} else {
+					Messages.PARTICLE_NOT_SELECTED.sendTo(sender);
+				}
+			} else {
+				Messages.INVALID_SENDER.sendTo(sender);
+			}
+			return;
+		}
+
 		String name = args.get(0);
 		Optional<ParticleEffect> particle = plugin.getParticleManager().getParticle(name);
 
@@ -25,12 +43,11 @@ public class ParticleSpawn implements Command<Midnight> {
 		}
 
 		particle.get().startSpawning();
-		Messages.PARTICLE_SPAWN.sendTo(sender);
+		Messages.PARTICLE_SPAWN.sendTo(sender, Placeholders.of("{name}", Text.format(name)));
 	}
 
 	@Override
 	public void configureProperties(@Nonnull CommandProperties properties) {
 		properties.setUsage(Messages.PARTICLE_SPAWN_USAGE);
-		properties.setArgumentsRequired(1);
 	}
 }
