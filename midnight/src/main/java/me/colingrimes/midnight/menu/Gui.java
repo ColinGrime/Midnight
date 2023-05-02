713830d36e03ab88e458b79b612a2227232b54e9
Scill <realscill@gmail.com>
2023-05-01 22:55:27 -0400

    Combine into a singular abstract Gui class

--- midnight/src/main/java/me/colingrimes/midnight/menu/Gui.java
+++ midnight/src/main/java/me/colingrimes/midnight/menu/Gui.java
@@ -1,7 +1,10 @@
 package me.colingrimes.midnight.menu;
 
 import com.google.common.base.Preconditions;
+import me.colingrimes.midnight.menu.slot.SimpleSlot;
 import me.colingrimes.midnight.scheduler.Scheduler;
+import me.colingrimes.midnight.util.text.Text;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
@@ -11,33 +14,48 @@ import java.util.Map;
 
 /**
  * Represents a {@link Gui} menu in the game.
+ * Extend this class if you want basic menu functionalities.
  */
-public interface Gui {
+public abstract class Gui {
 
 	/**
 	 * Players currently viewing a {@link Gui} instance.
 	 */
-	Map<Player, Gui> players = new HashMap<>();
+	public static Map<Player, Gui> players = new HashMap<>();
+
+	private final Player player;
+	private final Inventory inventory;
+	private final Map<Integer, Slot> slots = new HashMap<>();
+	private boolean valid = false;
+
+	public Gui(@Nonnull Player player, @Nonnull String title, int rows) {
+		this.player = player;
+		this.inventory = Bukkit.createInventory(player, rows * 9, Text.color(title));
+	}
 
 	/**
 	 * Draws the {@link Gui} by placing all the items in the slots.
 	 * This method is also called when {@link Gui#open()} is run.
 	 */
-	void draw();
+	public abstract void draw();
 
 	/**
 	 * Gets the player viewing the {@link Gui}.
 	 * @return the player
 	 */
 	@Nonnull
-	Player getPlayer();
+	public Player getPlayer() {
+		return player;
+	}
 
 	/**
 	 * Gets the inventory that the {@link Gui} is representing.
 	 * @return the inventory
 	 */
 	@Nonnull
-	Inventory getHandle();
+	public Inventory getHandle() {
+		return inventory;
+	}
 
 	/**
 	 * Gets the {@link Slot} that corresponds to the slot number.
@@ -45,25 +63,37 @@ public interface Gui {
 	 * @return the slot object
 	 */
 	@Nonnull
-	Slot getSlot(int slot);
+	public Slot getSlot(int slot) {
+		Preconditions.checkArgument(slot < 0 || slot >= inventory.getSize(), "Invalid slot: " + slot);
+		return slots.computeIfAbsent(slot, i -> new SimpleSlot(this, i));
+	}
 
 	/**
 	 * Gets whether the {@link Gui} is valid (opened).
 	 * @return true if valid
 	 */
-	boolean isValid();
+	public boolean isValid() {
+		return valid;
+	}
 
 	/**
 	 * Invalidates a {@link Gui}.
 	 * This will prevent the menu from being interacted with further.
 	 */
-	void invalidate();
+	public void invalidate() {
+		valid = false;
+		Gui.players.remove(player);
+
+		// Wipe out the inventory to ensure it cannot be interacted with further.
+		inventory.clear();
+		slots.values().forEach(Slot::clearBindings);
+	}
 
 	/**
 	 * Opens the {@link Gui}.
 	 * To ensure it is properly loaded, there is a 1 tick delay in the opening.
 	 */
-	default void open() {
+	public void open() {
 		Preconditions.checkArgument(isValid(), "Gui has already been opened.");
 		draw();
 
@@ -82,7 +112,7 @@ public interface Gui {
 	/**
 	 * Closes the {@link Gui}.
 	 */
-	default void close() {
+	public void close() {
 		getPlayer().closeInventory();
 	}
 }
