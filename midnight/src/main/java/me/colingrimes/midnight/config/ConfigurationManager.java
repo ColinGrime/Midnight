f293497ddb9303f723598d4eb2aada33953fff83
Scill <realscill@gmail.com>
2023-05-07 12:21:57 -0400

    Add Javadoc comment

--- midnight/src/main/java/me/colingrimes/midnight/config/ConfigurationManager.java
+++ midnight/src/main/java/me/colingrimes/midnight/config/ConfigurationManager.java
@@ -4,6 +4,13 @@ import javax.annotation.Nonnull;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Manages the configurations of the current plugin.
+ * Configurations are automatically added to the manager when they are registered.
+ * <p>
+ * To reload all configurations of the current plugin, run {@link #reload()}.
+ * To reload a specific configuration of the current plugin, run {@link #reload(String)}.
+ */
 public class ConfigurationManager {
 
 	private final Map<String, ConfigurationState> configurations = new HashMap<>();
