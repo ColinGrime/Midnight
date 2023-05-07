21fc4dd4c31bc294c2ff10d62be9114f73b3d44e
Scill <realscill@gmail.com>
2023-05-07 01:01:23 -0400

    Add Postgres connection provider

---
+++ midnight/src/main/java/me/colingrimes/midnight/storage/sql/connection/hikari/PostgresConnectionProvider.java
@@ -0,0 +1,34 @@
+package me.colingrimes.midnight.storage.sql.connection.hikari;
+
+import com.zaxxer.hikari.HikariConfig;
+import me.colingrimes.midnight.storage.sql.connection.DatabaseCredentials;
+
+import javax.annotation.Nonnull;
+import java.util.function.Function;
+
+public class PostgresConnectionProvider extends HikariConnectionProvider {
+
+    public PostgresConnectionProvider(@Nonnull DatabaseCredentials credentials) {
+        super(credentials);
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "PostgreSQL";
+    }
+
+    @Nonnull
+    @Override
+    public Function<String, String> getStatementProcessor() {
+        return s -> s.replace('\'', '"');
+    }
+
+    @Override
+    protected void configureDatabase(@Nonnull HikariConfig config, @Nonnull DatabaseCredentials credentials) {
+        config.setDriverClassName("org.postgresql.Driver");
+        config.setJdbcUrl("jdbc:postgresql://" + credentials.getHost() + ":" + credentials.getPort() + "/" + credentials.getDatabase());
+        config.setUsername(credentials.getUser());
+        config.setPassword(credentials.getPassword());
+    }
+}
