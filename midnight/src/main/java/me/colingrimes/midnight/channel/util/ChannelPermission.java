07ec5f9704540beb4717544018e887ad46fb9e41
Scill <realscill@gmail.com>
2023-05-07 01:03:25 -0400

    Add ChannelPermission enum

---
+++ midnight/src/main/java/me/colingrimes/midnight/channel/util/ChannelPermission.java
@@ -0,0 +1,26 @@
+package me.colingrimes.midnight.channel.util;
+
+/**
+ * Represents permissions for different actions in a channel.
+ */
+public enum ChannelPermission {
+    /**
+     * Permission to join a channel.
+     */
+    JOIN,
+
+    /**
+     * Permission to leave a channel.
+     */
+    LEAVE,
+
+    /**
+     * Permission to send messages in a channel.
+     */
+    SEND,
+
+    /**
+     * Permission to mute/unmute other participants in a channel.
+     */
+    MUTE;
+}
