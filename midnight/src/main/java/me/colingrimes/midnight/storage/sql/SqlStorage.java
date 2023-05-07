625ab8456a0ac31f0afb31f1f5a04cb97eb84d7f
Scill <realscill@gmail.com>
2023-05-07 00:56:25 -0400

    Add Flyway support to manage migrations

--- midnight/src/main/java/me/colingrimes/midnight/storage/sql/SqlStorage.java
+++ midnight/src/main/java/me/colingrimes/midnight/storage/sql/SqlStorage.java
@@ -1,102 +1,41 @@
 package me.colingrimes.midnight.storage.sql;
 
-import me.colingrimes.midnight.MidnightPlugin;
-import me.colingrimes.midnight.serialize.Serializable;
 import me.colingrimes.midnight.storage.Storage;
 import me.colingrimes.midnight.storage.sql.connection.ConnectionProvider;
+import org.flywaydb.core.Flyway;
 
 import javax.annotation.Nonnull;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
 
 /**
  * An abstract class representing an SQL-based storage.
  * @param <T> the type of data being stored
  */
-public abstract class SqlStorage<T extends Serializable> implements Storage<T> {
+public abstract class SqlStorage<T> implements Storage<T> {
 
-    protected final MidnightPlugin plugin;
     protected final ConnectionProvider connectionProvider;
 
-    public SqlStorage(@Nonnull MidnightPlugin plugin, @Nonnull ConnectionProvider connectionProvider) {
-        this.plugin = plugin;
+    public SqlStorage(@Nonnull ConnectionProvider connectionProvider) {
         this.connectionProvider = connectionProvider;
     }
 
     @Override
     public void init() throws Exception {
-        connectionProvider.init();
-
-        if (!tableExists(getTable())) {
-            applySchema(getTable());
+        if (connectionProvider.isInitialized()) {
+            return;
+        } else {
+            connectionProvider.init();
         }
+
+        // Configure and run Flyway migrations.
+        Flyway.configure()
+                .dataSource(connectionProvider.getDataSource())
+                .locations("classpath:/migrations/" + connectionProvider.getName().toLowerCase())
+                .load()
+                .migrate();
     }
 
     @Override
     public void shutdown() {
         connectionProvider.shutdown();
-    }
-
-    /**
-     * Gets the table name.
-     * @return the table name
-     */
-    @Nonnull
-    protected abstract String getTable();
-
-    /**
-     * Checks whether a table with the given name exists in the connected database.
-     * The comparison is case-insensitive.
-     *
-     * @param tableName the name of the table to check for existence
-     * @return true if the table exists, false otherwise
-     * @throws SQLException if a database access error occurs
-     */
-    private boolean tableExists(@Nonnull String tableName) throws SQLException {
-        try (Connection connection = connectionProvider.getConnection()) {
-            try (ResultSet resultSet = connection.getMetaData().getTables(connection.getCatalog(), null, tableName, null)) {
-                while (resultSet.next()) {
-                    if (resultSet.getString("TABLE_NAME").equalsIgnoreCase(tableName)) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-        }
-    }
-
-    /**
-     * Applies the schema to the connected database.
-     * Reads an SQL schema file specific to the type of database being used
-     * and executes the queries to create the tables.
-     *
-     * @param schemaName the name of the schema file to read
-     * @throws IOException if there's an error reading the schema file
-     * @throws SQLException if a database access error occurs
-     */
-    private void applySchema(@Nonnull String schemaName) throws IOException, SQLException {
-        List<String> queries;
-        String path = "/schema/" + connectionProvider.getName().toLowerCase() + "/" + schemaName + ".sql";
-
-        try (InputStream inputStream = getClass().getResourceAsStream(path)) {
-            if (inputStream == null) {
-                throw new IOException("Schema file not found (" + path + ")");
-            }
-            queries = SchemaReader.getQueries(inputStream);
-        }
-
-        try (Connection connection = connectionProvider.getConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                for (String query : queries) {
-                    statement.addBatch(query);
-                }
-                statement.executeBatch();
-            }
-        }
     }
 }
