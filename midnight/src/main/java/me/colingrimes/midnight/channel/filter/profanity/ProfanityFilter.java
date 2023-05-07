7287bead1dc19935b5c0059d57e739918f5279b4
Scill <realscill@gmail.com>
2023-05-07 01:01:07 -0400

    Add various filters

---
+++ midnight/src/main/java/me/colingrimes/midnight/channel/filter/profanity/ProfanityFilter.java
@@ -0,0 +1,8 @@
+package me.colingrimes.midnight.channel.filter.profanity;
+
+import me.colingrimes.midnight.channel.filter.ChatFilter;
+
+/**
+ * An abstract chat filter that filters out messages containing profanity.
+ */
+public abstract class ProfanityFilter implements ChatFilter {}
