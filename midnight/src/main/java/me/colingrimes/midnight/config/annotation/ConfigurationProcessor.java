81230c96b2059753ced93727a9b435daa969c3a2
Scill <realscill@gmail.com>
2023-05-10 14:53:48 -0400

    Move all configuration registration logic to this class

---
+++ midnight/src/main/java/me/colingrimes/midnight/config/annotation/ConfigurationProcessor.java
@@ -0,0 +1,40 @@
+package me.colingrimes.midnight.config.annotation;
+
+import me.colingrimes.midnight.Midnight;
+import me.colingrimes.midnight.config.ConfigurationState;
+import me.colingrimes.midnight.config.adapter.ConfigurationAdapter;
+import me.colingrimes.midnight.config.option.Option;
+import me.colingrimes.midnight.util.io.Files;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ * Responsible for processing the {@link Configuration} annotation.
+ */
+public class ConfigurationProcessor {
+
+	private final Midnight plugin;
+
+	public ConfigurationProcessor(@Nonnull Midnight plugin) {
+		this.plugin = plugin;
+	}
+
+	/**
+	 * Processes all classes with the {@link Configuration} annotation.
+	 * <p>
+	 * Only classes in the "config" package will be processed.
+	 */
+	public void process() {
+		for (Class<?> clazz : Files.getClasses(plugin, plugin.getRootPackage() + ".config")) {
+			if (clazz.isAnnotationPresent(Configuration.class)) {
+				String configName = clazz.getAnnotation(Configuration.class).value();
+				ConfigurationAdapter adapter = ConfigurationAdapter.of(plugin, configName);
+				List<? extends Option<?>> options = Option.initialize(clazz);
+
+				ConfigurationState state = new ConfigurationState(adapter, options);
+				plugin.getConfigurationManager().addConfiguration(configName, state);
+			}
+		}
+	}
+}
