dd48b24322cf16845c1dae9bb9bcc08a240be0ac
Scill <realscill@gmail.com>
2023-05-11 20:47:57 -0400

    Add UUID methods and account for nulls

--- midnight/src/main/java/me/colingrimes/midnight/util/io/DatabaseUtils.java
+++ midnight/src/main/java/me/colingrimes/midnight/util/io/DatabaseUtils.java
@@ -4,6 +4,7 @@ import me.colingrimes.midnight.storage.sql.DatabaseType;
 
 import java.sql.*;
 import java.time.*;
+import java.util.UUID;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -12,25 +13,6 @@ import javax.annotation.Nullable;
  * Utility class for database operations.
  */
 public final class DatabaseUtils {
-
-	/**
-	 * Set a timestamp in a PreparedStatement in a way that works for all database types.
-	 *
-	 * @param ps             the preparedStatement in which to set the timestamp
-	 * @param parameterIndex the index in the PreparedStatement at which to set the timestamp
-	 * @param dateTime       the ZonedDateTime to set, may be null
-	 * @param type           the type of the database
-	 * @throws SQLException if a database access error occurs.
-	 */
-	public static void setTimestamp(@Nonnull PreparedStatement ps, int parameterIndex, @Nullable ZonedDateTime dateTime, @Nonnull DatabaseType type) throws SQLException {
-		if (dateTime == null) {
-			ps.setString(parameterIndex, null);
-		} else if (type == DatabaseType.SQLITE) {
-			ps.setString(parameterIndex, dateTime.withZoneSameInstant(ZoneId.of("UTC")).toInstant().toString());
-		} else {
-			ps.setTimestamp(parameterIndex, Timestamp.from(dateTime.withZoneSameInstant(ZoneId.of("UTC")).toInstant()));
-		}
-	}
 
 	/**
 	 * Get a timestamp from a ResultSet in a way that works for all database types.
@@ -44,11 +26,77 @@ public final class DatabaseUtils {
 	@Nullable
 	public static ZonedDateTime getTimestamp(@Nonnull ResultSet resultSet, @Nonnull String columnName, @Nonnull DatabaseType type) throws SQLException {
 		if (type == DatabaseType.SQLITE) {
+			Logger.log("THIS ONE");
 			String dateTimeStr = resultSet.getString(columnName);
 			return dateTimeStr != null ? ZonedDateTime.parse(dateTimeStr).withZoneSameInstant(ZoneId.systemDefault()) : null;
 		} else {
 			Timestamp timestamp = resultSet.getTimestamp(columnName);
 			return timestamp != null ? ZonedDateTime.ofInstant(timestamp.toInstant(), ZoneId.systemDefault()) : null;
+		}
+	}
+
+	/**
+	 * Set a timestamp in a PreparedStatement in a way that works for all database types.
+	 *
+	 * @param ps             the preparedStatement in which to set the timestamp
+	 * @param parameterIndex the index in the PreparedStatement at which to set the timestamp
+	 * @param dateTime       the ZonedDateTime to set, may be null
+	 * @param type           the type of the database
+	 * @throws SQLException if a database access error occurs.
+	 */
+	public static void setTimestamp(@Nonnull PreparedStatement ps, int parameterIndex, @Nullable ZonedDateTime dateTime, @Nonnull DatabaseType type) throws SQLException {
+		if (dateTime == null) {
+			if (type == DatabaseType.SQLITE) {
+				ps.setString(parameterIndex, null);
+			} else {
+				ps.setNull(parameterIndex, java.sql.Types.TIMESTAMP);
+			}
+		} else if (type == DatabaseType.SQLITE) {
+			ps.setString(parameterIndex, dateTime.withZoneSameInstant(ZoneId.of("UTC")).toInstant().toString());
+		} else {
+			ps.setTimestamp(parameterIndex, Timestamp.from(dateTime.withZoneSameInstant(ZoneId.of("UTC")).toInstant()));
+		}
+	}
+
+	/**
+	 * Get a UUID from a ResultSet in a way that works for all database types.
+	 *
+	 * @param resultSet  the ResultSet from which to get the UUID
+	 * @param columnName the name of the column from which to get the UUID
+	 * @param type       the type of the database
+	 * @return the UUID represented by the string or object, may be null
+	 * @throws SQLException if a database access error occurs.
+	 */
+	@Nullable
+	public static UUID getUUID(@Nonnull ResultSet resultSet, @Nonnull String columnName, @Nonnull DatabaseType type) throws SQLException {
+		if (type == DatabaseType.POSTGRESQL) {
+			return (UUID) resultSet.getObject(columnName);
+		} else {
+			String uuidStr = resultSet.getString(columnName);
+			return uuidStr != null ? UUID.fromString(uuidStr) : null;
+		}
+	}
+
+	/**
+	 * Set a UUID in a PreparedStatement in a way that works for all database types.
+	 *
+	 * @param ps             the preparedStatement in which to set the UUID
+	 * @param parameterIndex the index in the PreparedStatement at which to set the UUID
+	 * @param uuid           the UUID to set, may be null
+	 * @param type           the type of the database
+	 * @throws SQLException if a database access error occurs.
+	 */
+	public static void setUUID(@Nonnull PreparedStatement ps, int parameterIndex, @Nullable UUID uuid, @Nonnull DatabaseType type) throws SQLException {
+		if (uuid == null) {
+			if (type == DatabaseType.SQLITE) {
+				ps.setString(parameterIndex, null);
+			} else {
+				ps.setNull(parameterIndex, java.sql.Types.OTHER);
+			}
+		} else if (type == DatabaseType.POSTGRESQL) {
+			ps.setObject(parameterIndex, uuid);
+		} else {
+			ps.setString(parameterIndex, uuid.toString());
 		}
 	}
 
