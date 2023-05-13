404d3ca503f49fe5d3c2271c91f9fc25a5b7d9f1
Scill <realscill@gmail.com>
2023-05-13 10:10:34 -0400

    Make advertise filter stronger

--- midnight-channels/src/main/java/me/colingrimes/channels/channel/misc/filter/AdvertiseFilter.java
+++ midnight-channels/src/main/java/me/colingrimes/channels/channel/misc/filter/AdvertiseFilter.java
@@ -10,18 +10,19 @@ import java.util.Optional;
 import java.util.regex.Pattern;
 
 /**
- * A chat filter that filters out messages containing URLs.
+ * A chat filter that filters out messages containing URLs and IP addresses.
  */
 public class AdvertiseFilter implements ChatFilter {
 
-    private final Pattern urlPattern = Pattern.compile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))");
+    private final Pattern urlPattern = Pattern.compile("^(https?:\\/\\/)?([\\w\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w\\.-]*)*\\/?$");
+    private final Pattern ipPattern = Pattern.compile("^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$");
 
     @Override
     public boolean filter(@Nonnull ChannelMessage<?> message) {
         Optional<Chatter> chatter = message.getChatter();
         String content = message.toText();
 
-        if (chatter.isPresent() && urlPattern.matcher(content).find()) {
+        if (chatter.isPresent() && (urlPattern.matcher(content).find() || ipPattern.matcher(content).find())) {
             chatter.filter(Chatter::online).ifPresent(c -> Settings.ADVERTISING_WARNING.send(c.player()));
             return true;
         } else {
