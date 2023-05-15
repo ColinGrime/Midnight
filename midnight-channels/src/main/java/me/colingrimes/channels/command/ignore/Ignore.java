beaf4a62ac681e31117dc8baf855037e548680ec
Scill <realscill@gmail.com>
2023-05-15 19:01:10 -0400

    Add ignore + unignore commands

---
+++ midnight-channels/src/main/java/me/colingrimes/channels/command/ignore/Ignore.java
@@ -0,0 +1,48 @@
+package me.colingrimes.channels.command.ignore;
+
+import me.colingrimes.channels.MidnightChannels;
+import me.colingrimes.channels.channel.chatter.Chatter;
+import me.colingrimes.channels.config.Messages;
+import me.colingrimes.midnight.command.Command;
+import me.colingrimes.midnight.command.handler.util.ArgumentList;
+import me.colingrimes.midnight.command.handler.util.CommandProperties;
+import me.colingrimes.midnight.command.handler.util.Sender;
+import me.colingrimes.midnight.util.bukkit.Players;
+import me.colingrimes.midnight.util.misc.UUIDs;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class Ignore implements Command<MidnightChannels> {
+
+	@Override
+	public void execute(@Nonnull MidnightChannels plugin, @Nonnull Sender sender, @Nonnull ArgumentList args) {
+		Optional<UUID> uuid = UUIDs.fromName(args.get(0));
+		if (uuid.isPresent()) {
+			Chatter.of(sender.player()).ignore(uuid.get());
+			Messages.IGNORED.replace("{player}", args.get(0)).send(sender);
+		} else {
+			Messages.PLAYER_NOT_FOUND.send(sender);
+		}
+	}
+
+	@Nullable
+	@Override
+	public List<String> tabComplete(@Nonnull MidnightChannels plugin, @Nonnull Sender sender, @Nonnull ArgumentList args) {
+		if (args.size() == 1) {
+			return Players.mapList(Player::getName);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public void configureProperties(@Nonnull CommandProperties properties) {
+		properties.setArgumentsRequired(1);
+		properties.setPlayerRequired(true);
+	}
+}
