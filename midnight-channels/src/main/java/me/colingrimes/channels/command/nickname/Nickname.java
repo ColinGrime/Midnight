fe8c202499b5d37f753573a6bc9e8f507ef14750
Scill <realscill@gmail.com>
2023-05-15 19:01:15 -0400

    Add nickname command

---
+++ midnight-channels/src/main/java/me/colingrimes/channels/command/nickname/Nickname.java
@@ -0,0 +1,37 @@
+package me.colingrimes.channels.command.nickname;
+
+import me.colingrimes.channels.MidnightChannels;
+import me.colingrimes.channels.channel.chatter.Chatter;
+import me.colingrimes.channels.config.Messages;
+import me.colingrimes.midnight.command.Command;
+import me.colingrimes.midnight.command.handler.util.ArgumentList;
+import me.colingrimes.midnight.command.handler.util.CommandProperties;
+import me.colingrimes.midnight.command.handler.util.Sender;
+
+import javax.annotation.Nonnull;
+
+public class Nickname implements Command<MidnightChannels> {
+
+	@Override
+	public void execute(@Nonnull MidnightChannels plugin, @Nonnull Sender sender, @Nonnull ArgumentList args) {
+        String nickname = args.get(0);
+
+        if (nickname.length() > 16) {
+            Messages.NICKNAME_TOO_LONG.send(sender);
+        } else if (nickname.length() < 3) {
+			Messages.NICKNAME_TOO_SHORT.send(sender);
+        } else if (!nickname.matches("[a-zA-Z0-9_]+")) {
+            Messages.NICKNAME_NOT_ALPHANUMERIC.send(sender);
+        } else {
+			Chatter.of(sender.player()).setNickname(nickname);
+			Messages.NICKNAME_CHANGED.replace("{nickname}", nickname).send(sender);
+		}
+	}
+
+	@Override
+	public void configureProperties(@Nonnull CommandProperties properties) {
+		properties.setUsage(Messages.NICKNAME_USAGE);
+		properties.setArgumentsRequired(1);
+		properties.setPlayerRequired(true);
+	}
+}
