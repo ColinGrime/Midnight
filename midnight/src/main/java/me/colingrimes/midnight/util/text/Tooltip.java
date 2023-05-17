97bf4b57e404b397456442b427f414db7fa3c61a
Scill <realscill@gmail.com>
2023-05-17 19:02:34 -0400

    Add PlaceholderAPI support

--- midnight/src/main/java/me/colingrimes/midnight/util/text/Tooltip.java
+++ midnight/src/main/java/me/colingrimes/midnight/util/text/Tooltip.java
@@ -1,8 +1,10 @@
 package me.colingrimes.midnight.util.text;
 
+import me.colingrimes.midnight.message.Placeholders;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -19,10 +21,27 @@ public final class Tooltip {
      * @param tooltips the list of strings to be added as hoverable text
      * @return a TextComponent with the message and hoverable text
      */
+    @Nonnull
     public static TextComponent create(@Nonnull String message, @Nonnull List<String> tooltips) {
         TextComponent component = new TextComponent(message);
         component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(String.join("\n", tooltips))));
         return component;
+    }
+
+    /**
+     * Creates a TextComponent with hoverable text.
+     * Supports placeholders from PlaceholderAPI.
+     *
+     * @param player   the player to replace placeholders for
+     * @param message  the message to be displayed
+     * @param tooltips the list of strings to be added as hoverable text
+     * @return a TextComponent with the message and hoverable text
+     */
+    @Nonnull
+    public static TextComponent create(@Nonnull Player player, @Nonnull String message, @Nonnull List<String> tooltips) {
+        TextComponent component = new TextComponent(message);
+        component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(String.join("\n", tooltips))));
+        return Placeholders.create(player).apply(component);
     }
 
     private Tooltip() {
