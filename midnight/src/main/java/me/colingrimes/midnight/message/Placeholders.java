868cc70bf0af38ee52fe4a3e47a8405e153cb77d
Scill <realscill@gmail.com>
2023-05-19 18:45:27 -0400

    Huge refactor on Placeholders class

--- midnight/src/main/java/me/colingrimes/midnight/message/Placeholders.java
+++ midnight/src/main/java/me/colingrimes/midnight/message/Placeholders.java
@@ -1,19 +1,20 @@
 package me.colingrimes.midnight.message;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import me.colingrimes.midnight.message.implementation.ComponentMessage;
+import me.colingrimes.midnight.message.implementation.ListMessage;
+import me.colingrimes.midnight.message.implementation.TextMessage;
 import me.colingrimes.midnight.util.Common;
-import me.colingrimes.midnight.util.text.Text;
+import me.colingrimes.midnight.util.text.Component;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * A utility class designed for managing and applying multiple placeholders
@@ -23,7 +24,7 @@ import java.util.stream.Collectors;
  */
 public class Placeholders {
 
-	private final Map<String, String> placeholders = new HashMap<>();
+	private final Map<String, Message<?>> placeholders = new HashMap<>();
 	private Player player;
 
 	/**
@@ -64,6 +65,18 @@ public class Placeholders {
 	}
 
 	/**
+	 * Creates a new placeholders object.
+	 *
+	 * @param placeholder the placeholder you want to add
+	 * @param value       the value you want to replace the placeholder with
+	 * @return the placeholders object
+	 */
+	@Nonnull
+	public static Placeholders of(@Nonnull String placeholder, @Nonnull Message<?> value) {
+		return new Placeholders().add(placeholder, value);
+	}
+
+	/**
 	 * Adds a placeholder to the list of placeholders.
 	 *
 	 * @param placeholder the placeholder you want to add
@@ -73,7 +86,20 @@ public class Placeholders {
 	 */
 	@Nonnull
 	public <T> Placeholders add(@Nonnull String placeholder, @Nonnull T value) {
-		placeholders.put(placeholder, String.valueOf(value));
+		placeholders.put(placeholder, Message.of(String.valueOf(value)));
+		return this;
+	}
+
+	/**
+	 * Adds a placeholder to the list of placeholders.
+	 *
+	 * @param placeholder the placeholder you want to add
+	 * @param value       the value you want to replace the placeholder with
+	 * @return the placeholders object
+	 */
+	@Nonnull
+	public Placeholders add(@Nonnull String placeholder, @Nonnull Message<?> value) {
+		placeholders.put(placeholder, value);
 		return this;
 	}
 
@@ -84,22 +110,20 @@ public class Placeholders {
 	 * @return the new string with applied placeholders
 	 */
 	@Nonnull
-	public String apply(@Nullable String str) {
+	public ComponentMessage apply(@Nullable String str) {
 		if (str == null) {
-			return "";
+			return new ComponentMessage();
 		}
 
 		// Apply the placeholders in the string.
-		for (Map.Entry<String, String> replacement : placeholders.entrySet()) {
-			str = str.replace(replacement.getKey(), replacement.getValue());
+		TextComponent component = new TextComponent(str);
+		for (Map.Entry<String, Message<?>> replacement : placeholders.entrySet()) {
+			component = Component.replace(component, replacement.getKey(), replacement.getValue());
 		}
 
 		// Apply PlaceholderAPI placeholders.
-		if (Common.getPlugin("PlaceholderAPI") != null) {
-			str = PlaceholderAPI.setPlaceholders(player, str.replaceAll("\\{(.+?)}", "%$1%"));
-		}
-
-		return Text.color(str.replaceAll("%(.+?)%", "{$1}"));
+		applyPlaceholderAPI(component);
+		return new ComponentMessage(component);
 	}
 
 	/**
@@ -109,12 +133,16 @@ public class Placeholders {
 	 * @return the new list of strings with applied placeholders
 	 */
 	@Nonnull
-	public List<String> apply(@Nullable List<String> strList) {
+	public ComponentMessage apply(@Nullable List<String> strList) {
 		if (strList == null) {
-			return new ArrayList<>();
+			return new ComponentMessage();
 		}
 
-		return strList.stream().map(this::apply).collect(Collectors.toList());
+		ComponentMessage component = apply(strList.get(0));
+		for (int i=1; i<strList.size(); i++) {
+			component.getContent().addExtra(apply(strList.get(i)).getContent());
+		}
+		return component;
 	}
 
 	/**
@@ -124,34 +152,36 @@ public class Placeholders {
 	 * @return the new component with applied placeholders
 	 */
 	@Nonnull
-	public TextComponent apply(@Nullable TextComponent component) {
+	public ComponentMessage apply(@Nullable TextComponent component) {
 		if (component == null) {
-			return new TextComponent();
+			return new ComponentMessage();
 		}
 
 		// Apply the placeholders in the main component.
-		TextComponent mainComponent = new TextComponent(apply(component.getText()));
+		ComponentMessage message = apply(component.getText());
+		TextComponent mainComponent = message.getContent();
 		mainComponent.setClickEvent(component.getClickEvent());
 		mainComponent.setHoverEvent(component.getHoverEvent());
 
 		if (component.getExtra() == null) {
-			return mainComponent;
+			return message;
 		}
 
 		// Apply the placeholders in the extra components.
-		for (BaseComponent base : component.getExtra()) {
-			if (!(base instanceof TextComponent extraComponent)) {
-				mainComponent.addExtra(base);
+		for (BaseComponent baseComponent : component.getExtra()) {
+			if (!(baseComponent instanceof TextComponent)) {
+				mainComponent.addExtra(baseComponent);
 				continue;
 			}
 
-			TextComponent newTextComponent = new TextComponent(apply(extraComponent.getText()));
-			newTextComponent.setClickEvent(extraComponent.getClickEvent());
-			newTextComponent.setHoverEvent(extraComponent.getHoverEvent());
-			mainComponent.addExtra(newTextComponent);
+			ComponentMessage extraMessage = apply(((TextComponent) baseComponent).getText());
+			TextComponent extraComponent = extraMessage.getContent();
+			extraComponent.setClickEvent(extraComponent.getClickEvent());
+			extraComponent.setHoverEvent(extraComponent.getHoverEvent());
+			mainComponent.addExtra(extraComponent);
 		}
 
-		return mainComponent;
+		return message;
 	}
 
 	/**
@@ -160,14 +190,40 @@ public class Placeholders {
 	 * @param message the message to apply placeholders to
 	 * @return the new message with applied placeholders
 	 */
+	@SuppressWarnings("unchecked")
 	@Nonnull
-	public Message<?> apply(@Nonnull Message<?> message) {
-		if (message.getContent() instanceof String) {
-			return Message.of(apply((String) message.getContent()));
-		} else if (message.getContent() instanceof TextComponent) {
-			return Message.of(apply((TextComponent) message.getContent()));
+	public ComponentMessage apply(@Nonnull Message<?> message) {
+		if (message instanceof TextMessage) {
+			return apply((String) message.getContent());
+		} else if (message instanceof ComponentMessage) {
+			return apply((TextComponent) message.getContent());
+		} else if (message instanceof ListMessage) {
+			return apply((List<String>) message.getContent());
 		} else {
-			return message;
+			throw new IllegalArgumentException("Invalid message content type: " + message.getContent().getClass().getName());
+		}
+	}
+
+	/**
+	 * Applies PlaceholderAPI placeholders to a component.
+	 *
+	 * @param component the component to apply placeholders to
+	 */
+	private void applyPlaceholderAPI(@Nonnull TextComponent component) {
+		if (Common.getPlugin("PlaceholderAPI") == null) {
+			return;
+		}
+
+		component.setText(PlaceholderAPI.setPlaceholders(player, component.getText().replaceAll("\\{(.+?)}", "%$1%")));
+		component.setText(component.getText().replaceAll("%(.+?)%", "{$1}"));
+
+		// Recursively replace placeholders in the extras of the original component.
+		if (component.getExtra() != null) {
+			for (BaseComponent extra : component.getExtra()) {
+				if (extra instanceof TextComponent extraComponent) {
+					applyPlaceholderAPI(extraComponent);
+				}
+			}
 		}
 	}
 }
