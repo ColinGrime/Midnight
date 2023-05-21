f7462b4429b6f39dd4fa740fc7d2299b3e166256
Scill <realscill@gmail.com>
2023-05-20 21:46:39 -0400

    Add sender tooltip and command

--- midnight-channels/src/main/java/me/colingrimes/channels/channel/misc/ChannelSettings.java
+++ midnight-channels/src/main/java/me/colingrimes/channels/channel/misc/ChannelSettings.java
@@ -10,8 +10,11 @@ import me.colingrimes.midnight.message.Message;
 import me.colingrimes.midnight.message.Placeholders;
 import me.colingrimes.midnight.scheduler.Scheduler;
 import me.colingrimes.midnight.util.io.Logger;
+import me.colingrimes.midnight.util.text.Tooltip;
 
 import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Represents the settings for a communication channel.
@@ -20,6 +23,13 @@ public class ChannelSettings implements ChatFilter {
 
     private final ChatFilters filters = new ChatFilters();
     private Message<?> messageFormat = Message.of("&7<&f&lMember&7> &e{sender}&7: &f{message}");
+    private List<String> senderTooltip = Arrays.asList(
+            "&8&m----&8[ &a{sender} &8]&m----",
+            "&eRank: &a{vault_prefix}",
+            "&eBalance: &a${vault_eco_balance}",
+            "&8&m--------------",
+            "&7&oClick to message!");
+    private String senderClickCommand = "/msg {sender} ";
     private boolean logMessages = false;
 
     @Override
@@ -91,7 +101,8 @@ public class ChannelSettings implements ChatFilter {
     public Message<?> getFormattedMessage(@Nonnull Chatter chatter, @Nonnull Message<?> message, @Nonnull String sender) {
         return Placeholders.create(chatter.player())
                 .add("{message}", message.toText())
-                .add("{sender}", sender)
+                .add("{sender}", Tooltip.create(chatter.player(), sender, senderTooltip, senderClickCommand))
+                .add("{nickname}", chatter.getNickname())
                 .apply(messageFormat);
     }
 
@@ -122,6 +133,53 @@ public class ChannelSettings implements ChatFilter {
     public ChannelSettings setMessageFormat(@Nonnull Message<?> format) {
         this.messageFormat = format;
         return this;
+    }
+
+    /**
+     * Gets the sender tooltip for the channel.
+     *
+     * @return the sender tooltip
+     */
+    @Nonnull
+    public List<String> getSenderTooltip() {
+        return senderTooltip;
+    }
+
+    /**
+     * Sets the sender tooltip for the channel.
+     *
+     * @param senderTooltip the sender tooltip
+     */
+    public void setSenderTooltip(@Nonnull List<String> senderTooltip) {
+        this.senderTooltip = senderTooltip;
+    }
+
+    /**
+     * Sets the sender tooltip for the channel.
+     *
+     * @param senderTooltip the sender tooltip
+     */
+    public void setSenderTooltip(@Nonnull String... senderTooltip) {
+        this.senderTooltip = Arrays.asList(senderTooltip);
+    }
+
+    /**
+     * Gets the command to run when the sender is clicked.
+     *
+     * @return the command to run
+     */
+    @Nonnull
+    public String getSenderClickCommand() {
+        return senderClickCommand;
+    }
+
+    /**
+     * Sets the command to run when the sender is clicked.
+     *
+     * @param senderClickCommand the command to run
+     */
+    public void setSenderClickCommand(@Nonnull String senderClickCommand) {
+        this.senderClickCommand = senderClickCommand;
     }
 
     /**
