6edb072320118e9778989c254dbc2d08d8f05f39
Scill <realscill@gmail.com>
2023-05-20 21:46:13 -0400

    Add command suggestion

--- midnight/src/main/java/me/colingrimes/midnight/util/text/Tooltip.java
+++ midnight/src/main/java/me/colingrimes/midnight/util/text/Tooltip.java
@@ -2,6 +2,7 @@ package me.colingrimes.midnight.util.text;
 
 import me.colingrimes.midnight.message.Placeholders;
 import me.colingrimes.midnight.message.implementation.ComponentMessage;
+import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
@@ -30,6 +31,22 @@ public final class Tooltip {
     }
 
     /**
+     * Creates a TextComponent with hoverable text and a command to suggest when the text is clicked.
+     *
+     * @param message  the message to be displayed
+     * @param tooltips the list of strings to be added as hoverable text
+     * @param command  the command to be run when the text is clicked
+     * @return a TextComponent with the message and hoverable text
+     */
+    @Nonnull
+    public static TextComponent create(@Nonnull String message, @Nonnull List<String> tooltips, @Nonnull String command) {
+        TextComponent component = new TextComponent(message);
+        component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(String.join("\n", tooltips))));
+        component.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command));
+        return component;
+    }
+
+    /**
      * Creates a TextComponent with hoverable text.
      * Supports placeholders from PlaceholderAPI.
      *
@@ -42,6 +59,24 @@ public final class Tooltip {
     public static ComponentMessage create(@Nonnull Player player, @Nonnull String message, @Nonnull List<String> tooltips) {
         TextComponent component = new TextComponent(message);
         component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(String.join("\n", tooltips))));
+        return Placeholders.create(player).apply(component);
+    }
+
+    /**
+     * Creates a TextComponent with hoverable text and a command to suggest when the text is clicked.
+     * Supports placeholders from PlaceholderAPI.
+     *
+     * @param player   the player to replace placeholders for
+     * @param message  the message to be displayed
+     * @param tooltips the list of strings to be added as hoverable text
+     * @param command  the command to be run when the text is clicked
+     * @return a TextComponent with the message and hoverable text
+     */
+    @Nonnull
+    public static ComponentMessage create(@Nonnull Player player, @Nonnull String message, @Nonnull List<String> tooltips, @Nonnull String command) {
+        TextComponent component = new TextComponent(message);
+        component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(String.join("\n", tooltips))));
+        component.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command));
         return Placeholders.create(player).apply(component);
     }
 
