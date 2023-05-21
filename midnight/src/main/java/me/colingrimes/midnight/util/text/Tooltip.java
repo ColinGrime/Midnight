b3618d82cc261e329f7b12569ba2aabfabdd6e4f
Scill <realscill@gmail.com>
2023-05-21 11:46:58 -0400

    Clean up methods

--- midnight/src/main/java/me/colingrimes/midnight/util/text/Tooltip.java
+++ midnight/src/main/java/me/colingrimes/midnight/util/text/Tooltip.java
@@ -1,12 +1,10 @@
 package me.colingrimes.midnight.util.text;
 
-import me.colingrimes.midnight.message.Placeholders;
 import me.colingrimes.midnight.message.implementation.ComponentMessage;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
-import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -17,21 +15,21 @@ import java.util.List;
 public final class Tooltip {
 
     /**
-     * Creates a TextComponent with hoverable text.
+     * Creates a {@link ComponentMessage} with hoverable text.
      *
      * @param message  the message to be displayed
      * @param tooltips the list of strings to be added as hoverable text
      * @return a TextComponent with the message and hoverable text
      */
     @Nonnull
-    public static TextComponent create(@Nonnull String message, @Nonnull List<String> tooltips) {
+    public static ComponentMessage create(@Nonnull String message, @Nonnull List<String> tooltips) {
         TextComponent component = new TextComponent(message);
         component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(String.join("\n", tooltips))));
-        return component;
+        return new ComponentMessage(component);
     }
 
     /**
-     * Creates a TextComponent with hoverable text and a command to suggest when the text is clicked.
+     * Creates a {@link ComponentMessage} with hoverable text and a command to suggest when the text is clicked.
      *
      * @param message  the message to be displayed
      * @param tooltips the list of strings to be added as hoverable text
@@ -39,45 +37,11 @@ public final class Tooltip {
      * @return a TextComponent with the message and hoverable text
      */
     @Nonnull
-    public static TextComponent create(@Nonnull String message, @Nonnull List<String> tooltips, @Nonnull String command) {
+    public static ComponentMessage create(@Nonnull String message, @Nonnull List<String> tooltips, @Nonnull String command) {
         TextComponent component = new TextComponent(message);
         component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(String.join("\n", tooltips))));
         component.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command));
-        return component;
-    }
-
-    /**
-     * Creates a TextComponent with hoverable text.
-     * Supports placeholders from PlaceholderAPI.
-     *
-     * @param player   the player to replace placeholders for
-     * @param message  the message to be displayed
-     * @param tooltips the list of strings to be added as hoverable text
-     * @return a TextComponent with the message and hoverable text
-     */
-    @Nonnull
-    public static ComponentMessage create(@Nonnull Player player, @Nonnull String message, @Nonnull List<String> tooltips) {
-        TextComponent component = new TextComponent(message);
-        component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(String.join("\n", tooltips))));
-        return Placeholders.create(player).apply(component);
-    }
-
-    /**
-     * Creates a TextComponent with hoverable text and a command to suggest when the text is clicked.
-     * Supports placeholders from PlaceholderAPI.
-     *
-     * @param player   the player to replace placeholders for
-     * @param message  the message to be displayed
-     * @param tooltips the list of strings to be added as hoverable text
-     * @param command  the command to be run when the text is clicked
-     * @return a TextComponent with the message and hoverable text
-     */
-    @Nonnull
-    public static ComponentMessage create(@Nonnull Player player, @Nonnull String message, @Nonnull List<String> tooltips, @Nonnull String command) {
-        TextComponent component = new TextComponent(message);
-        component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(String.join("\n", tooltips))));
-        component.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command));
-        return Placeholders.create(player).apply(component);
+        return new ComponentMessage(component);
     }
 
     private Tooltip() {
