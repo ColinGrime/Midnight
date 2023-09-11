d4af518a4e3415129134387bddac9e2549870835
Scill <realscill@gmail.com>
2023-09-10 22:03:24 -0400

    Add bind method + clean up

--- midnight/src/main/java/me/colingrimes/midnight/menu/pattern/MultiPattern.java
+++ midnight/src/main/java/me/colingrimes/midnight/menu/pattern/MultiPattern.java
@@ -2,23 +2,31 @@ package me.colingrimes.midnight.menu.pattern;
 
 import com.google.common.base.Preconditions;
 import me.colingrimes.midnight.menu.Gui;
+import me.colingrimes.midnight.menu.Slot;
 import me.colingrimes.midnight.util.bukkit.Items;
 import org.bukkit.Material;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Consumer;
 
 /**
  * Represents a pattern of slots in a {@link Gui} that can be filled with different items.
  */
 public class MultiPattern {
 
+    public static final MultiPattern TWO_CHOICES = MultiPattern.create()
+            .mask("LLLLMRRRR")
+            .mask("LLLLMRRRR")
+            .mask("LLLLMRRRR")
+            .mask("LLLLMRRRR");
+
     private final List<String> patterns;
     private final Map<Character, ItemStack> items;
+    private final Map<Character, Map<ClickType, Set<Consumer<InventoryClickEvent>>>> handlers;
 
     /**
      * Creates a new MultiPattern instance.
@@ -33,6 +41,7 @@ public class MultiPattern {
     private MultiPattern() {
         this.patterns = new ArrayList<>();
         this.items = new HashMap<>();
+        this.handlers = new HashMap<>();
     }
 
     /**
@@ -40,12 +49,13 @@ public class MultiPattern {
      *
      * @param mask the pattern mask, using '0' for empty slots and any other character for slots to be filled
      * @return this MultiPattern instance for chaining
+     * @throws IllegalArgumentException if the mask is too large
      * @throws IllegalStateException if more than 6 lines have been added to the pattern
      */
     @Nonnull
     public MultiPattern mask(@Nonnull String mask) {
-        Preconditions.checkArgument(mask.length() == 9, "The pattern is too large to fit in the Gui.");
-        Preconditions.checkState(patterns.size() < 6, "A MultiPattern cannot have more than 6 lines.");
+        Preconditions.checkArgument(mask.length() <= 9, "Mask is too large to fit in the Gui.");
+        Preconditions.checkState(patterns.size() < 6, "MultiPattern cannot have more than 6 lines.");
         patterns.add(mask);
         return this;
     }
@@ -56,6 +66,7 @@ public class MultiPattern {
      * @param c the character to map to the item
      * @param item the item to place in the slots represented by the character in the pattern
      * @return this MultiPattern instance for chaining
+     * @throws IllegalArgumentException if the character is a reserved character ('0')
      */
     @Nonnull
     public MultiPattern item(char c, @Nonnull ItemStack item) {
@@ -77,26 +88,35 @@ public class MultiPattern {
     }
 
     /**
+     * Binds a click type to a handler that will perform an action on click.
+     * The handler will be applied to the specified character.
+     *
+     * @param c the character to map to the handler
+     * @param type the click type
+     * @param handler the action to perform on the click
+     * @return this MultiPattern instance for chaining
+     */
+    @Nonnull
+    public MultiPattern bind(char c, @Nonnull ClickType type, @Nonnull Consumer<InventoryClickEvent> handler) {
+        Map<ClickType, Set<Consumer<InventoryClickEvent>>> handlers = this.handlers.computeIfAbsent(c, __ -> new HashMap<>());
+        handlers.computeIfAbsent(type, __ -> new HashSet<>()).add(handler);
+        return this;
+    }
+
+    /**
      * Fills the slots in the {@link Gui} based on the current pattern and mapped items.
      *
      * @param gui the {@link Gui} in which to fill the slots
-     * @throws IllegalArgumentException if the pattern is too large to fit in the Gui
-     * @throws IllegalStateException if a character in the pattern is not mapped to an item
      */
     public void fill(@Nonnull Gui gui) {
-        for (String line : patterns) {
-            Preconditions.checkArgument(line.length() <= 9, "The pattern is too large to fit in the Gui.");
-        }
-
-        // Fills the slots in the Gui based on the pattern.
         for (int i=0; i<patterns.size(); i++) {
             String row = patterns.get(i);
             for (int j=0; j<row.length(); j++) {
                 char c = row.charAt(j);
                 if (c != '0') {
-                    Preconditions.checkState(items.containsKey(c), "Character '" + c + "' in the pattern is not mapped to an item.");
-                    int slot = i * 9 + j;
-                    gui.getSlot(slot).setItem(items.get(c));
+                    Slot slot = gui.getSlot(i * 9 + j);
+                    slot.setItem(items.computeIfAbsent(c, __ -> Items.of(Material.STONE).name("&cNo item mapped.").build()));
+                    handlers.get(c).forEach((type, handlers) -> handlers.forEach(h -> slot.bind(type, h)));
                 }
             }
         }
--- midnight/src/main/java/me/colingrimes/midnight/menu/pattern/Pattern.java
+++ midnight/src/main/java/me/colingrimes/midnight/menu/pattern/Pattern.java
@@ -2,13 +2,16 @@ package me.colingrimes.midnight.menu.pattern;
 
 import com.google.common.base.Preconditions;
 import me.colingrimes.midnight.menu.Gui;
+import me.colingrimes.midnight.menu.Slot;
 import me.colingrimes.midnight.util.bukkit.Items;
 import org.bukkit.Material;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
 
 /**
  * Represents a pattern of slots in a {@link Gui} that can be filled with an item.
@@ -24,6 +27,7 @@ public class Pattern {
             .mask("111111111");
 
     private final List<String> patterns;
+    private final Map<ClickType, Set<Consumer<InventoryClickEvent>>> handlers;
     private ItemStack item;
 
     /**
@@ -60,6 +64,7 @@ public class Pattern {
 
     private Pattern() {
         this.patterns = new ArrayList<>();
+        this.handlers = new HashMap<>();
     }
 
     /**
@@ -67,14 +72,14 @@ public class Pattern {
      *
      * @param mask the pattern mask, using '0' for empty slots and '1' for slots to be filled
      * @return this Pattern instance for chaining
+     * @throws IllegalArgumentException if the mask is too large, or it contains characters other than '0' and '1'
      * @throws IllegalStateException if more than 6 lines have been added to the pattern
-     * @throws IllegalArgumentException if the mask contains characters other than '0' and '1'
      */
     @Nonnull
     public Pattern mask(@Nonnull String mask) {
-        Preconditions.checkArgument(mask.length() <= 9, "The pattern is too large to fit in the Gui.");
+        Preconditions.checkArgument(mask.length() <= 9, "Mask is too large to fit in the Gui.");
         Preconditions.checkArgument(mask.chars().allMatch(c -> c == '0' || c == '1'), "Mask can only contain '0' and '1' characters.");
-        Preconditions.checkState(patterns.size() < 6, "A Pattern cannot have more than 6 lines.");
+        Preconditions.checkState(patterns.size() < 6, "Pattern cannot have more than 6 lines.");
         patterns.add(mask);
         return this;
     }
@@ -103,21 +108,36 @@ public class Pattern {
     }
 
     /**
+     * Binds a click type to a handler that will perform an action on click.
+     * The handler will be applied to all filled slots.
+     *
+     * @param type the click type
+     * @param handler the action to perform on the click
+     * @return this Pattern instance for chaining
+     */
+    @Nonnull
+    public Pattern bind(@Nonnull ClickType type, @Nonnull Consumer<InventoryClickEvent> handler) {
+        handlers.computeIfAbsent(type, __ -> new HashSet<>()).add(handler);
+        return this;
+    }
+
+    /**
      * Fills the slots in the {@link Gui} based on the current pattern and item.
      *
      * @param gui the {@link Gui} in which to fill the slots
-     * @throws IllegalArgumentException if the pattern is too large to fit in the Gui
      */
     public void fill(@Nonnull Gui gui) {
+        if (item == null) {
+            item = Items.of(Material.STONE).name("&cNo item mapped.").build();
+        }
 
-
-        // Fills the slots in the Gui based on the pattern.
-        for (int i = 0; i< patterns.size(); i++) {
+        for (int i=0; i<patterns.size(); i++) {
             String row = patterns.get(i);
             for (int j=0; j<row.length(); j++) {
                 if (row.charAt(j) == '1') {
-                    int slot = i * 9 + j;
-                    gui.getSlot(slot).setItem(item);
+                    Slot slot = gui.getSlot(i * 9 + j);
+                    slot.setItem(item);
+                    handlers.forEach((type, handlers) -> handlers.forEach(h -> slot.bind(type, h)));
                 }
             }
         }
